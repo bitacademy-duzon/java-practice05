7e601ff1046aa4a80295b515c6b1ed12d47ce4ca
kicks <kickscar@gmail.com>
2019-01-04 06:17:33 +0900

    finished

--- src/prob1/Sort.java
+++ src/prob1/Sort.java
@@ -16,7 +16,22 @@ public class Sort {
 		
 		//
 		// 정렬 알고리즘이 적용된 코드를 여기에 작성합니다.
-		// 
+		//
+		for( int i = 0; i < count; i++ ) {
+			for( int j = 0; j < count-1-i; j++ ) {
+				if( array[ j ] < array[ j + 1 ] ) {
+					temp = array[ j ];
+					array[ j ] = array[ j + 1 ];
+					array[ j + 1 ] = temp;
+				}
+			}
+		}
+		
+		System.out.println( "\nAfter Sort." );
+		
+		for (int i = 0; i < count; i++) {
+			System.out.print(array[i] + " ");
+		}		
 
 	}
 }
