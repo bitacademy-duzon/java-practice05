624cb175eb4277b13fc29f371a9bfc1ea9e45407
CoRock <dycorphin@gmail.com>
2019-01-03 16:59:24 +0900

    Add problem 2

--- src/prob2/SmartPhone.java
+++ src/prob2/SmartPhone.java
@@ -1,4 +1,29 @@
 package prob2;
 
 public class SmartPhone extends MusicPhone {
+
+	@Override
+	public void execute(String function) {
+		if ("음악".equals(function)) {
+			playMusic();
+			return;
+		}
+		
+		if ("앱".equals(function)) {
+			playApp();
+			return;
+		}
+		
+		super.execute(function);
+	}
+	
+	@Override
+	protected void playMusic() {
+		System.out.println("다운로드해서 음악재생");
+	}
+	
+	public void playApp() {
+		System.out.println("앱실행");
+	}
+	
 }
